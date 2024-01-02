24a89cc3445b1929520dd1b0d822efa7465cbe4e
sirisayshello <veckovis.salongen0p@icloud.com>
2024-01-02 11:34:30 +0100

    added rooms

--- rooms.php
+++ rooms.php
@@ -8,6 +8,7 @@ require __DIR__ . '/roomFunctions.php';
 $checkIn = $_POST['arrivalDate'];
 $checkOut = $_POST['departureDate'];
 
+$allRooms = getRooms();
 $availableRooms = availableRooms($checkIn, $checkOut);
 
 ?>
@@ -24,26 +25,58 @@ $availableRooms = availableRooms($checkIn, $checkOut);
 
 <body>
     <main>
-        <section class="room-container">
-            <div class="room-container-inner">
+        <section class="section-container">
+            <form method="post" action="/booking.php">
+                <div class="room-container">
+                    <div class="room-container-inner">
 
-                <?php
 
-                // Present available rooms
-                foreach ($availableRooms as $room) :
+                        <?php
 
-                ?>
-                    <label class="room-check">
-                        <input type="radio" name="room" hidden>
-                        <div class="room-card">
-                            <h6><?= $room['name'] ?></h6>
+                        // Present available rooms
+                        foreach ($allRooms as $room) :
+                            $isAvailable = isRoomAvailable($room['id'], $availableRooms);
 
+                        ?>
+                            <label class="room-check">
+                                <input type="radio" name="room" value="<?= $room['id'] ?>" required hidden <?= $isAvailable ? '' : 'disabled' ?>>
+                                <div class="room-card <?= $isAvailable ? '' : 'booked' ?>">
+
+                                    <img src="<?= $room['imageUrl'] ?>" alt="a hotelroom">
+                                    <h3><?= $room['name'] ?></h3>
+
+                                </div>
+
+                            </label>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="room-info">
+                        <h2>chosen room</h2>
+                        <p>Our most spacious room for 2-4 guests. Bedroom with one king-size bed. Separate sitting area with sofa-bed suitable for 2 children. Bathroom with shower, some rooms with bathtub.</p>
+                        <p>Includes</p>
+                        <p>Breakfast</p>
+                        <p>Access to private beach</p>
+                        <div>
+                            <h2>Enhance your stay</h2>
+                            <p>Paw-decure</p>
+                            <p>Bring human</p>
                         </div>
+                        <h3>Total cost: </h3>
+                    </div>
+                    <div class="booking-details">
+                        <label for="arrivalDate">Arrival date</label>
+                        <input id="arrivalDate" value="<?= $checkIn ?>" type="date" min="2024-01-01" max="2024-01-31" name="arrivalDate" readonly />
+                        <label for="departureDate">Departure date</label>
+                        <input id="departureDate" value="<?= $checkOut ?>" type="date" min="2024-01-01" max="2024-01-31" name="departureDate" readonly />
 
-                    </label>
-                <?php endforeach; ?>
-            </div>
+                        <input type="text" placeholder="First name" name="firstName" required />
+                        <input type="text" placeholder="Last name" name="lastName" required />
+                        <input type="text" placeholder="transfer code" name="transferCode" required />
+                        <button class="button" type="submit">Book your stay</button>
+                    </div>
+                </div>
 
+            </form>
         </section>
     </main>
 </body>
