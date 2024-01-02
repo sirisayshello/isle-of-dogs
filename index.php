0035d15990f0d5a830a37311bfc0128542be05e5
sirisayshello <veckovis.salongen0p@icloud.com>
2024-01-02 11:36:13 +0100

    updated index.php

--- index.php
+++ index.php
@@ -12,32 +12,36 @@ declare(strict_types=1);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Belmond Cockapoo Palace</title>
     <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 </head>
 
 <body>
+    <header class="header">
+        <div class="title">
+            <h1>BELMOND COCKAPOO PALACE</h1>
+            <h2>(NOT) A BELMOND HOTEL</h2>
+            <H3>ISLE OF DOGS</H3>
+        </div>
+    </header>
     <main>
-        <h1>Isle of dogs</h1>
-        <h2>Belmond Cockapoo Palace</h2>
+        <section class="intro">
+            <div class="border"></div>
+            <img src="images/facade.jpg" alt="facade of hotel building">
+            <section class="intro-booking">
+                <div>
+                    <h2>BOOK A ROOM</h2>
+                </div>
 
-        <div>
-            <h3>Book a room</h3>
-            <p>Here you can check for available rooms</p>
-        </div>
+                <form method="post" action="/rooms.php">
+                    <label for="arrivalDate">Arrival date</label>
+                    <input id="arrivalDate" type="date" min="2024-01-01" max="2024-01-31" name="arrivalDate" />
+                    <label for="departureDate">Departure date</label>
+                    <input id="departureDate" type="date" min="2024-01-01" max="2024-01-31" name="departureDate" />
+                    <button class="button" type="submit">Search for available rooms</button>
+                </form>
 
-        <div>
-            <form method="post" action="/rooms.php">
-                <label for="arrivalDate">Arrival date</label>
-                <input id="arrivalDate" type="date" min="2024-01-01" max="2024-01-31" name="arrivalDate" />
-                <label for="departureDate">Departure date</label>
-                <input id="departureDate" type="date" min="2024-01-01" max="2024-01-31" name="departureDate" />
-                <button type="submit">Search for available rooms</button>
-            </form>
-        </div>
-        <input type="text" placeholder="First name" name="firstName" />
-        <input type="text" placeholder="Last name" name="lastName" />
-        <div>
-
-        </div>
+            </section>
+        </section>
     </main>
 </body>
 
