75a3d56b57802ef080af9276a1ab6054b533cc2e
sirisayshello <veckovis.salongen0p@icloud.com>
2024-01-09 22:44:18 +0100

    explore page

---
+++ explore.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Belmond Cockapoo Palace</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="index.php">
+            <div class="title">
+                <h1>BELMOND COCKAPOO PALACE</h1>
+                <h3>(NOT) A BELMOND HOTEL</h3>
+                <h3>ISLE OF DOGS</h3>
+            </div>
+        </a>
+    </header>
+    <section class="menu">
+        <div class="menu-bar">
+            <a href="viewRooms.php">ROOMS</a>
+            <a href="explore.php">EXPLORE</a>
+        </div>
+    </section>
+    <main>
+        <section class="section-container">
+            <section class="amenities">
+                <section class="amenities-card">
+                    <img class="explore-img" src="/images/beach.jpg" alt="">
+                    <div class="amenities-info">
+                        <h1>Beach Service</h1>
+                        <p>Head down to the world-famous sands of Cockapoo Beach while still enjoying our particular brand of pampering. Our exclusive service is seasonal and based on availability. All hotel guests can relax with sun loungers, umbrellas and water.
+                            <br>
+                            <br>
+                            Beach assistants will also look after your belongings if you choose to dive into the azure waters.
+                        </p>
+                    </div>
+                </section>
+                <section class="amenities-card-2nd">
+                    <div class="amenities-info">
+                        <h1>Swimming Pool</h1>
+                        <p>Sunbathe, swim and socialize in one of Isle of dogs’s largest and most attractive swimming pools. Pool attendants will be on hand throughout the day to cater to your every request.
+                            <br>
+                            <br>
+                            Relax on the stylish pool furniture, or order cocktails and tapas from the Pool Bar menu.
+                            <br>
+                            <br>
+                            Open daily, 7am-7pm
+                        </p>
+                    </div>
+                    <img class="explore-img" src="/images/pool.jpg" alt="">
+                </section>
+                <section class="amenities-card">
+                    <img class="explore-img" src="/images/tennis.jpg" alt="tennis racket leaning on white bench">
+                    <div class="amenities-info">
+                        <h1>Tennis</h1>
+                        <p>Keep fit or de-stress with a game of tennis in our courts. Speak to our concierge to arrange for free use of racquets and balls. Alternatively improve your serve or work on your backhand with the help of a professional coach.</p>
+                    </div>
+                </section>
+            </section>
+        </section>
+    </main>
+
+</body>
+
+</html>
